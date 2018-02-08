9ead07173d97e20c09ff9007cc77c7039b2571a1
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-02-08 21:11:48 +0900

    Add tests for ripper wrapper

--- lib/iro.rb
+++ lib/iro.rb
@@ -1,4 +1,7 @@
+require 'ripper'
+
 require "iro/version"
+require 'iro/ripper_wrapper'
 
 module Iro
   # Your code goes here...
---
+++ test/iro/ripper_wrapper_test.rb
@@ -0,0 +1,69 @@
+require 'test_helper'
+
+class TestIroRipperWrapper < Minitest::Test
+  using Iro::RipperWrapper
+
+  def expr(ruby)
+    Ripper.sexp(ruby)[1][0]
+  end
+
+  def test_type
+    sexp = expr('foo.bar')
+    assert_equal :call, sexp.type
+    sexp = expr('[1,2,3]')
+    assert_equal :array, sexp.type
+    sexp = expr('1')
+    assert_equal :@int, sexp.type
+  end
+
+  def test_children
+    sexp = expr('[1,2,3]')
+    assert_equal [[:@int, "1", [1, 1]], [:@int, "2", [1, 3]], [:@int, "3", [1, 5]]], sexp.children
+  end
+
+  def test_content
+    sexp = expr('1234')
+    assert_equal "1234", sexp.content
+  end
+
+  def test_position
+    sexp = expr("\n 1234 ")
+    assert_equal [2, 1], sexp.position
+  end
+
+  def test_node?
+    sexp = expr("foo.bar")
+    assert sexp.node?
+
+    sexp = expr("1234")
+    refute sexp.position.node?
+  end
+
+  # XXX: Is it correct?
+  def test_parser_event?
+    sexp = expr("1")
+    assert sexp.parser_event?
+
+    sexp = expr("foo.bar")
+    refute sexp.parser_event?
+  end
+
+  # XXX: Is it correct?
+  def test_scanner_event?
+    sexp = expr("foo.bar")
+    assert sexp.scanner_event?
+
+    sexp = expr("1")
+    refute sexp.scanner_event?
+  end
+
+  def test_XXX_type?
+    sexp = expr('foo.bar')
+    assert sexp.call_type?
+    refute sexp.vcall_type?
+
+    sexp = expr('123')
+    assert sexp.int_type?
+    refute sexp.float_type?
+  end
+end
