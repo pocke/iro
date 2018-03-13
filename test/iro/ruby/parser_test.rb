2f94818ec61056247680fa06fba403b5d8bd98be
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-03-13 09:25:10 +0900

    Refactor test

--- test/iro/ruby/parser_test.rb
+++ test/iro/ruby/parser_test.rb
@@ -1,192 +1,167 @@
 require 'test_helper'
 
 class TestIroRubyParser < Minitest::Test
-  def parse(source)
-    Iro::Ruby::Parser.tokens(source)
+  def assert_parse(expected, source)
+    got = Iro::Ruby::Parser.tokens(source)
+    assert_equal(expected, got)
   end
 
   def test_tokens_string
-    tokens = parse(<<~RUBY)
-      "foo"
-    RUBY
-
-    assert_equal(
+    assert_parse(
       {
         'Delimiter' => [[1,1,1], [1,5,1]],
         'String' => [[1,2,3]]
-      },
-      tokens
+      }, <<~RUBY
+        "foo"
+      RUBY
     )
   end
 
   def test_tokens_backtick
-    tokens = parse(<<~RUBY)
-      `ls -a`
-    RUBY
-
-    assert_equal(
+    assert_parse(
       {
         'Delimiter' => [[1,1,1], [1,7,1]],
         'String' => [[1,2,5]]
-      },
-      tokens
+      }, <<~RUBY
+        `ls -a`
+      RUBY
     )
   end
 
   def test_tokens_label
-    tokens = parse(<<~RUBY)
-      {foo: bar}
-      def a foo: bar
-      end
-    RUBY
-
-    assert_equal(
+    assert_parse(
       {
         'rubySymbol' => [[1, 2, 4], [2, 7, 4]],
         'rubyDefine' => [[2, 1, 3]],
         'Keyword' => [[3, 1, 3]],
         'rubyFunction' => [[2, 5, 1]],
-      },
-      tokens
+      }, <<~RUBY
+        {foo: bar}
+        def a foo: bar
+        end
+      RUBY
     )
   end
 
   def test_tokens_ivar
-    tokens = parse(<<~RUBY)
-      @foo = a
-      p @foo
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         'rubyInstanceVariable' => [[1, 1, 4], [2, 3, 4]],
-      },
-      tokens
+      }, <<~RUBY
+        @foo = a
+        p @foo
+      RUBY
     )
   end
 
   def test_tokens_cvar
-    tokens = parse(<<~RUBY)
-      @@foo = a
-      p @@foo
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         'rubyClassVariable' => [[1, 1, 5], [2, 3, 5]],
-      },
-      tokens
+      }, <<~RUBY
+        @@foo = a
+        p @@foo
+      RUBY
     )
   end
 
   def test_tokens_gvar
-    tokens = parse(<<~RUBY)
-      $foo = a
-      p $foo
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         'rubyGlobalVariable' => [[1, 1, 4], [2, 3, 4]],
-      },
-      tokens
+      }, <<~RUBY
+        $foo = a
+        p $foo
+      RUBY
     )
   end
 
   def test_tokens_lvar
-    tokens = parse(<<~RUBY)
-      p x
-      x = 1
-      p x
-    RUBY
-
-    assert_equal(
+    assert_parse(
       {
         'Number' => [[2,5,1]],
         'rubyLocalVariable' => [[3, 3, 1]],
-      },
-      tokens
+      }, <<~RUBY
+        p x
+        x = 1
+        p x
+      RUBY
     )
   end
 
   def test_tokens_kw
-    tokens = parse(<<~RUBY)
-      if foo
-      end
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "Keyword" => [[1, 1, 2], [2, 1, 3]]
-      },
-      tokens
+      }, <<~RUBY
+        if foo
+        end
+      RUBY
     )
 
-    tokens = parse(<<~RUBY)
-      def foo
-      end
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "rubyDefine" => [[1, 1, 3]],
         "Keyword" => [[2, 1, 3]],
         "rubyFunction" => [[1, 5, 3]],
-      },
-      tokens
+      }, <<~RUBY
+        def foo
+        end
+      RUBY
     )
   end
 
   def test_defs
-    tokens = parse(<<~RUBY)
-      def self.foo
-      end
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "rubyDefine" => [[1, 1, 3]],
         "Keyword" => [[2, 1, 3]],
         "rubyFunction" => [[1, 10, 3]],
-      },
-      tokens
+      }, <<~RUBY
+        def self.foo
+        end
+      RUBY
     )
   end
 
   def test_def_def
-    tokens = parse(<<~RUBY)
-      def def
-      end
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "rubyDefine" => [[1, 1, 3]],
         "Keyword" => [[2, 1, 3]],
         "rubyFunction" => [[1, 5, 3]],
-      },
-      tokens
+      }, <<~RUBY
+        def def
+        end
+      RUBY
     )
   end
 
   def test_symbol_literal
-    tokens = parse(<<~RUBY)
-      :foo
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "rubySymbol" => [[1, 2, 3]],
         "rubySymbolDelimiter" => [[1, 1, 1]]
-      },
-      tokens
+      }, <<~RUBY
+        :foo
+      RUBY
     )
   end
 
   def test_symbol_with_Xvar
-    tokens = parse(<<~RUBY)
-      :$foo
-      :@foo
-      :@@foo
-    RUBY
-    assert_equal(
+    assert_parse(
       {
         "rubySymbol" => [[1, 2, 4], [2, 2, 4], [3, 2, 5]],
-        "rubySymbolDelimiter" => [[1, 1, 1], [2, 1, 1], [3, 1, 1]]
-      },
-      tokens.reject { |_, v| v.empty? }
+        "rubySymbolDelimiter" => [[1, 1, 1], [2, 1, 1], [3, 1, 1]],
+        # FIXME
+        "rubyGlobalVariable" => [],
+        "rubyInstanceVariable" => [],
+        "rubyClassVariable" => [],
+      }, <<~RUBY
+        :$foo
+        :@foo
+        :@@foo
+      RUBY
     )
   end
 end
